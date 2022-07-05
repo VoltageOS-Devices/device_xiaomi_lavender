d11c7cdf5141ba8cd155b6b4ff22ddcb7e92ed2a
Vivekachooz <vivekachooz@gmail.com>
2022-07-05 20:56:35 +0530

    lavender: Don't set dalvik props according to total ram
    Change-Id: I4bd87d420354051d587938de7c5431f7be602546
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -41,38 +41,6 @@
 #include "vendor_init.h"
 #include "property_service.h"
 
-char const *heapstartsize;
-char const *heapgrowthlimit;
-char const *heapsize;
-char const *heapminfree;
-char const *heapmaxfree;
-char const *heaptargetutilization;
-
-void check_device()
-{
-    struct sysinfo sys;
-
-    sysinfo(&sys);
-
-    if (sys.totalram > 5072ull * 1024 * 1024) {
-        // from - phone-xhdpi-6144-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.5";
-        heapminfree = "8m";
-        heapmaxfree = "32m";
-    } else {
-        // from - phone-xxhdpi-4096-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.6";
-        heapminfree = "8m";
-        heapmaxfree = "16m";
-    }
-}
-
 void property_override(char const prop[], char const value[], bool add = true)
 {
     auto pi = (prop_info *) __system_property_find(prop);
@@ -86,15 +54,6 @@ void property_override(char const prop[], char const value[], bool add = true)
 
 void vendor_load_properties()
 {
-    check_device();
-
-    property_override("dalvik.vm.heapstartsize", heapstartsize);
-    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_override("dalvik.vm.heapsize", heapsize);
-    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
-    property_override("dalvik.vm.heapminfree", heapminfree);
-    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
-
     // Misc
     property_override("ro.apex.updatable", "false");
 }
