1a562ba3a5597f1bc18f9c5a485d8db27ac49287
Shreedhan003 <shridhan98@gmail.com>
2022-07-26 21:43:17 +0530

    lavender: FM: Always build FM

--- device.mk
+++ device.mk
@@ -204,13 +204,11 @@ PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
 
 # FM
-ifeq ($(BOARD_HAVE_QCOM_FM),true)
 PRODUCT_PACKAGES += \
     FM2 \
     libqcomfm_jni \
     qcom.fmradio \
     qcom.fmradio.xml
-endif
 
 # fwk-detect
 PRODUCT_PACKAGES += \
