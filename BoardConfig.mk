22127d34f93a2b86742725e9e0c6d29e16f83175
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-05-19 15:10:48 +0530

    lavender: Remove TARGET_ENABLE_MEDIADRM_64
    * It's only used when building drm@1.0 HIDL
    
    Change-Id: I06412e2dd2bb48d798be4ef6e9a26a17b65277a7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,9 +88,6 @@ TARGET_USES_GRALLOC1 := true
 TARGET_USES_ION := true
 TARGET_SCREEN_DENSITY := 420
 
-# DRM
-TARGET_ENABLE_MEDIADRM_64 := true
-
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
