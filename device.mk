d847d5c6dcb06ee8e3f1cfb200acaca555a87a93
Shreedhan003 <shridhan98@gmail.com>
2023-07-10 17:25:58 +0530

    Revert " lavender: Update DRM blobs from `SLD_sprout` [1/2]"
    This reverts commit 751c8027fd23bb630475d5a94446aba7b2ada902.

--- device.mk
+++ device.mk
@@ -185,12 +185,8 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0.vendor \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
